4cbeadc1ddaa17fd31abae0750101fadbabce5e9
David <davidlivadaru@gmail.com>
2018-01-21 14:08:57 +0200

    develop
    Add documentation to Predicate.

--- Sources/DLAngle/Model/Predicate.swift
+++ Sources/DLAngle/Model/Predicate.swift
@@ -8,32 +8,52 @@
 import Foundation
 import DLInterval
 
+/// A convenient type to check values.
 public struct Predicate {
     public typealias Closure = () -> Bool
     private let _predicate: Closure
 
+    /// Create a predicate with a closure.
+    ///
+    /// - Parameter predicate: The closure to use for predicate.
     public init(closure predicate: @escaping Closure) {
         _predicate = predicate
     }
 
+    /// Create a predicate which checks if a value is contained in an array.
+    ///
+    /// - Parameters:
+    ///   - array: The array of values.
+    ///   - value: The value to check.
     public init(array: [Double], value: Double) {
         _predicate = {
             return array.contains(value)
         }
     }
 
+    /// Create a predicate which checks if a value is contained within the bounds of an interval.
+    ///
+    /// - Parameters:
+    ///   - interval: The interval.
+    ///   - value: The value to check.
     public init<I: IntervalType>(interval: I, value: Double) {
         _predicate = {
             return interval.contains(value)
         }
     }
 
+    /// Create a predicate which negates other predicate.
+    ///
+    /// - Parameter predicate: The predicate to negate.
     public init(not predicate: Predicate) {
         _predicate = {
             return !predicate.evaluate()
         }
     }
 
+    /// Create a predicate which aggregates other predicates by applying *and* operation.
+    ///
+    /// - Parameter predicates: The predicated to aggregate.
     public init(and predicates: [Predicate]) {
         _predicate = {
             for predicate in predicates {
@@ -45,6 +65,9 @@ public struct Predicate {
         }
     }
 
+    /// Create a predicate which aggregates other predicates by applying *or* operation.
+    ///
+    /// - Parameter predicates: The predicated to aggregate.
     public init(or predicates: [Predicate]) {
         _predicate = {
             for predicate in predicates {
@@ -56,6 +79,9 @@ public struct Predicate {
         }
     }
 
+    /// Evalutes the predicate.
+    ///
+    /// - Returns: The result of evaluation.
     public func evaluate() -> Bool {
         return _predicate()
     }
